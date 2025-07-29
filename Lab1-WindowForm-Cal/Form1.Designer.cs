9ec0cbac1c3e7cc0e0860c6f6da2a466ebddbdeb
Jawad Mansoor <www.jawadmansoor37@gmail.com>
2025-07-30 01:48:08 +0500

    cmt

---
+++ Lab1-WindowForm-Cal/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace Project1
+{
+    partial class Calculator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            b7 = new Button();
+            b8 = new Button();
+            b9 = new Button();
+            b4 = new Button();
+            b5 = new Button();
+            b6 = new Button();
+            b1 = new Button();
+            b2 = new Button();
+            b3 = new Button();
+            b0 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            button18 = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            display = new RichTextBox();
+            button3 = new Button();
+            button19 = new Button();
+            button20 = new Button();
+            SuspendLayout();
+            // 
+            // b7
+            // 
+            b7.BackColor = SystemColors.ActiveCaption;
+            b7.FlatStyle = FlatStyle.Popup;
+            b7.Location = new Point(12, 271);
+            b7.Name = "b7";
+            b7.Size = new Size(94, 29);
+            b7.TabIndex = 3;
+            b7.Text = "7";
+            b7.UseVisualStyleBackColor = false;
+            b7.Click += b7_Click;
+            // 
+            // b8
+            // 
+            b8.BackColor = SystemColors.ActiveCaption;
+            b8.FlatStyle = FlatStyle.Popup;
+            b8.Location = new Point(122, 271);
+            b8.Name = "b8";
+            b8.Size = new Size(94, 29);
+            b8.TabIndex = 4;
+            b8.Text = "8";
+            b8.UseVisualStyleBackColor = false;
+            b8.Click += b8_Click;
+            // 
+            // b9
+            // 
+            b9.BackColor = SystemColors.ActiveCaption;
+            b9.FlatStyle = FlatStyle.Popup;
+            b9.Location = new Point(238, 271);
+            b9.Name = "b9";
+            b9.Size = new Size(94, 29);
+            b9.TabIndex = 5;
+            b9.Text = "9";
+            b9.UseVisualStyleBackColor = false;
+            b9.Click += b9_Click;
+            // 
+            // b4
+            // 
+            b4.BackColor = SystemColors.ActiveCaption;
+            b4.FlatStyle = FlatStyle.Popup;
+            b4.Location = new Point(12, 332);
+            b4.Name = "b4";
+            b4.Size = new Size(94, 29);
+            b4.TabIndex = 6;
+            b4.Text = "4";
+            b4.UseVisualStyleBackColor = false;
+            b4.Click += b4_Click;
+            // 
+            // b5
+            // 
+            b5.BackColor = SystemColors.ActiveCaption;
+            b5.FlatStyle = FlatStyle.Popup;
+            b5.Location = new Point(122, 332);
+            b5.Name = "b5";
+            b5.Size = new Size(94, 29);
+            b5.TabIndex = 7;
+            b5.Text = "5";
+            b5.UseVisualStyleBackColor = false;
+            b5.Click += b5_Click;
+            // 
+            // b6
+            // 
+            b6.BackColor = SystemColors.ActiveCaption;
+            b6.FlatStyle = FlatStyle.Popup;
+            b6.Location = new Point(238, 332);
+            b6.Name = "b6";
+            b6.Size = new Size(94, 29);
+            b6.TabIndex = 8;
+            b6.Text = "6";
+            b6.UseVisualStyleBackColor = false;
+            b6.Click += b6_Click;
+            // 
+            // b1
+            // 
+            b1.BackColor = SystemColors.ActiveCaption;
+            b1.FlatStyle = FlatStyle.Popup;
+            b1.Location = new Point(12, 397);
+            b1.Name = "b1";
+            b1.Size = new Size(94, 29);
+            b1.TabIndex = 9;
+            b1.Text = "1";
+            b1.UseVisualStyleBackColor = false;
+            b1.Click += b1_Click_1;
+            // 
+            // b2
+            // 
+            b2.BackColor = SystemColors.ActiveCaption;
+            b2.FlatStyle = FlatStyle.Popup;
+            b2.Location = new Point(122, 397);
+            b2.Name = "b2";
+            b2.Size = new Size(94, 29);
+            b2.TabIndex = 10;
+            b2.Text = "2";
+            b2.UseVisualStyleBackColor = false;
+            b2.Click += b2_Click;
+            // 
+            // b3
+            // 
+            b3.BackColor = SystemColors.ActiveCaption;
+            b3.FlatStyle = FlatStyle.Popup;
+            b3.Location = new Point(238, 397);
+            b3.Name = "b3";
+            b3.Size = new Size(94, 29);
+            b3.TabIndex = 11;
+            b3.Text = "3";
+            b3.UseVisualStyleBackColor = false;
+            b3.Click += b3_Click;
+            // 
+            // b0
+            // 
+            b0.BackColor = SystemColors.ActiveCaption;
+            b0.FlatStyle = FlatStyle.Popup;
+            b0.Location = new Point(122, 453);
+            b0.Name = "b0";
+            b0.Size = new Size(94, 29);
+            b0.TabIndex = 12;
+            b0.Text = "0";
+            b0.UseVisualStyleBackColor = false;
+            b0.Click += button13_Click;
+            // 
+            // button14
+            // 
+            button14.BackColor = SystemColors.ActiveCaption;
+            button14.FlatStyle = FlatStyle.Popup;
+            button14.Location = new Point(349, 213);
+            button14.Name = "button14";
+            button14.Size = new Size(61, 29);
+            button14.TabIndex = 13;
+            button14.Text = "/";
+            button14.UseVisualStyleBackColor = false;
+            button14.Click += button14_Click;
+            // 
+            // button15
+            // 
+            button15.BackColor = SystemColors.ActiveCaption;
+            button15.FlatStyle = FlatStyle.Popup;
+            button15.Location = new Point(349, 271);
+            button15.Name = "button15";
+            button15.Size = new Size(61, 29);
+            button15.TabIndex = 14;
+            button15.Text = "x";
+            button15.UseVisualStyleBackColor = false;
+            button15.Click += button15_Click;
+            // 
+            // button16
+            // 
+            button16.BackColor = SystemColors.ActiveCaption;
+            button16.FlatStyle = FlatStyle.Popup;
+            button16.Location = new Point(349, 332);
+            button16.Name = "button16";
+            button16.Size = new Size(61, 29);
+            button16.TabIndex = 15;
+            button16.Text = "-";
+            button16.UseVisualStyleBackColor = false;
+            button16.Click += button16_Click;
+            // 
+            // button17
+            // 
+            button17.BackColor = SystemColors.ActiveCaption;
+            button17.FlatStyle = FlatStyle.Popup;
+            button17.Location = new Point(349, 397);
+            button17.Name = "button17";
+            button17.Size = new Size(61, 29);
+            button17.TabIndex = 16;
+            button17.Text = "+";
+            button17.UseVisualStyleBackColor = false;
+            button17.Click += button17_Click;
+            // 
+            // button18
+            // 
+            button18.BackColor = SystemColors.ActiveCaption;
+            button18.FlatStyle = FlatStyle.Popup;
+            button18.Location = new Point(349, 453);
+            button18.Name = "button18";
+            button18.Size = new Size(61, 29);
+            button18.TabIndex = 17;
+            button18.Text = "=";
+            button18.UseVisualStyleBackColor = false;
+            button18.Click += button18_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Location = new Point(12, 453);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 18;
+            button1.Text = ".";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ActiveCaption;
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.Location = new Point(238, 453);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 19;
+            button2.Text = "esc";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // display
+            // 
+            display.Anchor = AnchorStyles.Right;
+            display.Location = new Point(12, 60);
+            display.Name = "display";
+            display.Size = new Size(398, 95);
+            display.TabIndex = 20;
+            display.Text = "0";
+            display.TextChanged += display_TextChanged;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ActiveCaption;
+            button3.FlatStyle = FlatStyle.Popup;
+            button3.Location = new Point(238, 213);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 21;
+            button3.Text = "%";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button19
+            // 
+            button19.BackColor = SystemColors.ActiveCaption;
+            button19.FlatStyle = FlatStyle.Popup;
+            button19.Location = new Point(12, 213);
+            button19.Name = "button19";
+            button19.Size = new Size(94, 29);
+            button19.TabIndex = 22;
+            button19.Text = "CE";
+            button19.UseVisualStyleBackColor = false;
+            button19.Click += button19_Click;
+            // 
+            // button20
+            // 
+            button20.BackColor = SystemColors.ActiveCaption;
+            button20.FlatStyle = FlatStyle.Popup;
+            button20.Location = new Point(122, 213);
+            button20.Name = "button20";
+            button20.Size = new Size(94, 29);
+            button20.TabIndex = 23;
+            button20.Text = "C";
+            button20.UseVisualStyleBackColor = false;
+            button20.Click += button20_Click;
+            // 
+            // Calculator
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(422, 494);
+            Controls.Add(button20);
+            Controls.Add(button19);
+            Controls.Add(button3);
+            Controls.Add(display);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(button18);
+            Controls.Add(button17);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(b0);
+            Controls.Add(b3);
+            Controls.Add(b2);
+            Controls.Add(b1);
+            Controls.Add(b6);
+            Controls.Add(b5);
+            Controls.Add(b4);
+            Controls.Add(b9);
+            Controls.Add(b8);
+            Controls.Add(b7);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Calculator";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Calculator";
+            Load += Calculator_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button b7;
+        private Button b8;
+        private Button b9;
+        private Button b4;
+        private Button b5;
+        private Button b6;
+        private Button b1;
+        private Button b2;
+        private Button b3;
+        private Button b0;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private Button button18;
+        private Button button1;
+        private Button button2;
+        private RichTextBox display;
+        private Button button3;
+        private Button button19;
+        private Button button20;
+    }
+}
