28e8780f52a05e0e9524a18283eec92339677405
Jawadyyy <www.jawadmansoor37@gmail.com>
2025-07-30 03:01:07 +0500

    cmt

---
+++ Lab7-Library-LocalDBSQL/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorBookLibrary.Models
+
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+    }
+}
---
+++ Lab7-Library-LocalDBSQL/Services/BookService.cs
@@ -0,0 +1,16 @@
+﻿using BlazorBookLibrary.Models;
+using System.Collections.Generic;
+
+namespace BlazorBookLibrary.Services {
+    public class BookService {
+
+        private readonly List<Book> _books = new ();
+
+        public List<Book> GetBooks() => _books;
+
+        public void IncBook(Book book) {
+                book.Id = _books.Count + 1;
+                _books.Add(book);
+        }
+    }
+}
