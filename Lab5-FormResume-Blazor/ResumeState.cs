6d0d0dc34ac6196642ff4ee6caf77e3ccc9ec08d
Jawadyyy <www.jawadmansoor37@gmail.com>
2025-07-30 02:53:43 +0500

    cmt

---
+++ Lab5-FormResume-Blazor/ResumeState.cs
@@ -0,0 +1,11 @@
+﻿public class ResumeStateService
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Address { get; set; }
+    public string Summary { get; set; }
+    public string Experience { get; set; }
+    public string Education { get; set; }
+    public string Skills { get; set; }
+}
